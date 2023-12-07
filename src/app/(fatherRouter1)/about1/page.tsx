68e2fe7bbbd471456b5c8418f0cddd7d317ddd2f
kunlunrepo <482976531@qq.com>
2023-12-07 15:41:57 +0800

    路由参数获取

--- src/app/(fatherRouter1)/about1/page.tsx
+++ src/app/(fatherRouter1)/about1/page.tsx
@@ -1,7 +1,18 @@
-function About() {
+"use client";
+
+import {useSearchParams} from "next/navigation";
+
+function About(props:any) {
+    const searchParams = useSearchParams();
+    const xd = searchParams.get("xd") as string;
+    const name = searchParams.get("name") as  string;
+    console.log(props.searchParams);
     return (
         <div  className="min-h-screen">
             <div>关于页面1</div>
+            <div>
+                personal1 页面传递过来的参数：{xd + name}
+            </div>
         </div>
     )
 }
--- src/app/(fatherRouter2)/personal1/page.tsx
+++ src/app/(fatherRouter2)/personal1/page.tsx
@@ -1,8 +1,26 @@
-function Person() {
+"use client" // 客户端组件内部
+
+import Link from "next/link";
+import {usePathname, useRouter} from "next/navigation";
+
+function Personal() {
+    const pathname = usePathname();
+    const router = useRouter();
+    const xd = "other";
     return (
         <div  className="min-h-screen">
-            <div>个人页面1</div>
+            {/*<Link href="/about1?xd=小滴课堂&name=老王">
+                去关于个人的页面
+            </Link>*/}
+            {/*<Link href={{
+                pathname: "/about1",
+                query: {xd: "小滴课堂", name: "老王" }
+            }}>
+                去关于个人的页面
+            </Link>*/}
+            <div onClick={() => router.push("/about1?xd=小滴课堂&name=老王")}>去关于个人的页面</div>
+            <div>个人的页面</div>
         </div>
     )
 }
-export default Person;
+export default Personal;
