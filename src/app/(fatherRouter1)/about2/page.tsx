a1b0802c2e9a79b2db188cd766bc2589445ce76f
kunlunrepo <482976531@qq.com>
2023-12-07 15:51:16 +0800

    发送请求

--- src/app/(fatherRouter1)/about2/page.tsx
+++ src/app/(fatherRouter1)/about2/page.tsx
@@ -1,7 +1,24 @@
-function About() {
+async function About() {
+    async function getData() {
+        const response = await fetch("https://api-v2.xdclass.net/api/rank/v1/hot_product");
+
+        if (!response.ok) {
+            throw new Error("未找到");
+        }
+        const toJson = await response.json();
+
+        if (!toJson.data) throw new Error("404");
+
+        return toJson.data;
+    }
+
+    const data = await getData();
+    console.log(data[0]);
+    const title = data[0].productTitle;
     return (
         <div  className="min-h-screen">
             <div>关于页面2</div>
+            <div>{title}</div>
         </div>
     )
 }
